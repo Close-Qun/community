7d4c005e5a36f00e53bbea334d042bd60bc4b4a5
Close-Qun <343335439@qq.com>
2021-04-08 18:02:07 +0800

    third commit

--- src/main/java/life/majiang/community/HelloController/AuthorizeController.java
+++ src/main/java/life/majiang/community/HelloController/AuthorizeController.java
@@ -1,12 +1,15 @@
 package life.majiang.community.HelloController;
 
 import life.majiang.community.dto.AccessTokenDto;
+import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author 陈亦铖
@@ -23,7 +26,7 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private  String redirectUri;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request) {
 
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
@@ -32,7 +35,14 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         accessTokenDto.setRedirect_uri(redirectUri);
 
-        githubProvider.getAccessToken(accessTokenDto);
-        return "index";
+        String accessToken=githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user=githubProvider.getUser(accessToken);
+        if (user!=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
+
     }
 }
--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -33,5 +33,21 @@ public class GithubProvider {
             return null;
     }
 
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+
+
+        } catch (IOException e) {
+        }
+        return null;
+    }
 
 }
